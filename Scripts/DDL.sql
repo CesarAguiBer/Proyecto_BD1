299ec247af233c095573e4e08ed65a607ae50f48
César Aguilar <cesaraguiber20@gmail.com>
2021-04-15 15:22:45 -0600

    Agrega sentencia para creación de la tabla de pedidos

--- Scripts/DDL.sql
+++ Scripts/DDL.sql
@@ -11,3 +11,12 @@ CREATE TABLE Estado(
     id_estado int PRIMARY KEY,
     descripcion varchar(100)
 );
+
+CREATE TABLE Pedido(
+    id_pedido bigint PRIMARY KEY,
+    id_cliente bigint NOT NULL,
+    id_estado int NOT NULL,
+    fecha_creacion timestamp DEFAULT NOW(),
+    FOREIGN KEY (id_cliente) REFERENCES Cliente(id_cliente),
+    FOREIGN KEY (id_estado) REFERENCES Estado(id_estado) 
+);
