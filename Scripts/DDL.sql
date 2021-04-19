d4e588098e460a054ac99322ac2c7673076da411
aaron17gomez <aaron17ramirez@gmail.com>
2021-04-18 21:53:40 -0600

    se agrego la tabla Empleado

--- Scripts/DDL.sql
+++ Scripts/DDL.sql
@@ -6,6 +6,13 @@ CREATE TABLE Cliente(
     telefono varchar(100) NOT NULL,
     direccion varchar(100) NOT NULL
 );
+CREATE TABLE Empleado(
+    id_empleado nvarchar(20) PRIMARY KEY,
+    nombre varchar(100) NOT NULL,
+    apellido varchar(100) NOT NULL,
+    correo varchar(100) NOT NULL,
+    telefono varchar(100) NOT NULL
+);
 
 CREATE TABLE Estado(
     id_estado int PRIMARY KEY,
