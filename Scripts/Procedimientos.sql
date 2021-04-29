fbb417975f35b387f6a97e5390353932f16eb53b
César Aguilar <cesaraguiber20@gmail.com>
2021-04-29 15:29:21 -0600

    Agrega procedimiento 3

--- Scripts/Procedimientos.sql
+++ Scripts/Procedimientos.sql
@@ -13,6 +13,7 @@ BEGIN
 	END IF;
 END$$
 DELIMITER ;
+
 # CALL pizzeriadb.sp_crear_pizza ('1', 'Pizza con Piña', 200.00 );
 
 # 2. Agregar ingrediente a una pizza
@@ -40,6 +41,53 @@ DELIMITER ;
 
 # CALL pizzeriadb.sp_agregar_ingrediente('1', '1', 20 );
 
+# 3 Realizar el Procedimiento Almacenado en el que una orden pasa de ser ordenada a aprobada. Recuerde que aquí debería de pasar una reducción de inventario.
+
+DELIMITER $$
+CREATE PROCEDURE pizzeriadb.sp_aprobar_pedido
+(IN sp_id_pedido bigint)
+BEGIN
+	DECLARE existe int DEFAULT(SELECT COUNT(*) FROM pizzeriadb.Pedido AS p WHERE p.id_pedido = sp_id_pedido);
+    DECLARE estado varchar(20) DEFAULT(SELECT p.id_estado FROM pizzeriadb.Pedido AS p WHERE p.id_pedido = sp_id_pedido);
+    
+     IF existe > 0 THEN
+		IF estado = '1' THEN
+			
+            UPDATE pizzeriadb.Pedido AS p SET p.id_estado = '2' WHERE p.id_pedido = sp_id_pedido;
+            
+            WITH tmp1 AS(
+				SELECT 
+					p.id_pedido AS 'pedido', 
+                    pp.id_pizza AS 'pizza', 
+                    pp.cantidad AS 'pizzas_ordenadas' , 
+                    pi.cantidad AS 'cantidad_ingrediente_Pizza' , 
+					pi.id_ingrediente AS 'ingrediente'
+				FROM 
+					pizzeriadb.Pedido AS p 
+					JOIN pizzeriadb.PedidoPizza AS pp ON p.id_pedido = pp.id_pedido
+					JOIN pizzeriadb.PizzaIngrediente AS pi ON pi.id_pizza = pp.id_pizza
+					WHERE p.id_pedido = sp_id_pedido
+            ) 
+            
+            UPDATE 
+				tmp1 
+                JOIN Ingrediente AS i ON i.id_ingrediente = tmp1.ingrediente 
+			SET 
+				i.stock = i.stock - (tmp1.pizzas_ordenadas * tmp1.cantidad_ingrediente_pizza)
+            ;
+            
+            SELECT 1 AS Successed, 'El pedido ha sido aprobado' AS MSG;
+        ELSE
+			SELECT 0 AS Successed, 'No se puede actualizar el pedido' AS MSG;
+        END IF;
+    ELSE
+		SELECT 0 AS Successed, 'El ID de pedido no existe' AS MSG;
+    END IF;
+    
+END$$
+DELIMITER ;
+
+# CALL pizzeriadb.sp_aprobar_pedido(20);
 
 # 5. Convertir una orden a Orden Entrega.
 
@@ -51,10 +99,9 @@ BEGIN
     DECLARE estado varchar(20) DEFAULT(SELECT p.id_estado FROM pizzeriadb.Pedido AS p WHERE p.id_pedido = sp_id_pedido);
     
     IF existe > 0 THEN
-		IF estado = '1' THEN
-			UPDATE pizzeriadb.Pedido AS p SET p.id_estado = '2' WHERE p.id_pedido = sp_id_pedido;
+		IF estado = '2' THEN
             INSERT INTO pizzeriadb.OrdenEntrega(id_pedido, id_empleado, id_pago, valor_pedido, costo_entrega, fecha_entrega) VALUES(sp_id_pedido, sp_id_empleado, sp_id_pago, sp_valor_pedido, sp_costo_entrega, '1000-01-01 00:00:00');
-            SELECT 1 AS Successed, 'Se actualizó el pedido y se generó la orden de entrega correctamente' AS MSG;
+            SELECT 1 AS Successed, 'Se generó la orden de entrega correctamente' AS MSG;
         ELSE
 			SELECT 0 AS Successed, 'No se puede actualizar el pedido' AS MSG;
         END IF;
@@ -66,7 +113,6 @@ DELIMITER ;
 
 # CALL pizzeriadb.sp_generar_orden(201,'1','2',120.00,20.00);
 
-																		   
 # 6. Actualizar la orden una vez ha sido entregada. (considera el caso en el que no se sabe si el cliente pagara con efectivo o no)
 
 DELIMITER $$
@@ -90,8 +136,8 @@ BEGIN
 END$$
 DELIMITER ;
 
-# CALL pizzeriadb.sp_actualizar_entrega(3);																		   
-																		   
+# CALL pizzeriadb.sp_actualizar_entrega(3);
+
 # 7. Agregar un nuevo cliente
 
 DELIMITER $$
@@ -110,5 +156,3 @@ END $$
 DELIMITER ;
 
 # CALL pizzeriadb.sp_agregar_cliente ('1', 'Juan', 'Pérez', 'juan.perez@gmail.com', '9999-9999', 'Col. Altamira');
-
-																		  
