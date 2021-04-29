ab904b1b49446e48242e2c6b0a1b687f90b38092
NicoleValeriano1 <nicolevaleriano0@gmail.com>
2021-04-29 10:15:56 -0600

    se agrego en procedimiento(Agregar ingrediente a una pizza)

--- Scripts/Procedimientos.sql
+++ Scripts/Procedimientos.sql
@@ -1,3 +1,29 @@
+# 2. Agregar ingrediente a una pizza
+
+DELIMITER $$
+CREATE PROCEDURE pizzeriadb.sp_agregar_ingrediente
+(IN sp_id_pizza varchar(20), IN sp_id_ingrediente varchar(20), IN sp_cantidad int)
+BEGIN
+	DECLARE pizza_existe int DEFAULT (SELECT COUNT(*) FROM pizzeriadb.Pizza AS p WHERE p.id_pizza = sp_id_pizza);
+	DECLARE ingrediente_existe int DEFAULT (SELECT COUNT(*) FROM pizzeriadb.Ingrediente AS i WHERE i.id_ingrediente = sp_id_ingrediente);
+    DECLARE existe int DEFAULT (SELECT COUNT(*) FROM pizzeriadb.PizzaIngrediente AS pi WHERE pi.id_pizza = sp_id_pizza AND pi.id_ingrediente = sp_id_ingrediente);
+    
+    IF pizza_existe > 0 AND ingrediente_existe > 0 THEN
+        IF existe > 0 THEN
+			SELECT 0 AS Successed, 'El ID enviado ya existe' AS MSG;
+		ELSE
+			INSERT INTO pizzeriadb.PizzaIngrediente VALUES(sp_id_pizza, sp_id_ingrediente, sp_cantidad);
+			SELECT 1 AS Successed, 'Tu registro fue almacenado' AS MSG;
+        END IF;
+    ELSE
+		SELECT 0 AS Successed, 'El ID enviado de pizza/ingrediente no existe' AS MSG;
+	END IF;
+END$$
+DELIMITER ;
+
+# CALL pizzeriadb.sp_agregar_ingrediente('1', '1', 20 );
+
+
 # 5. Convertir una orden a Orden Entrega.
 
 DELIMITER $$
